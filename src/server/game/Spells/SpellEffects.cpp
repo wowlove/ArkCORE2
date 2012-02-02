3bff517a88e06ff0f3ad0075046463fc2df1d945
burnham <georgiosburnham@gmail.com>
2012-02-02 18:55:51 +0800

    Scripts/Spells: Fix Argent Tournament mount spells:
     * Break-Shield
     * Charge
     * Defend (visual)
     * Mounted duel
     * Faction Pennants when riding argent tournament mounts
    
    - Including cleanup in spell_generic
    
    by Souler froom TC https://github.com/TrinityCore/TrinityCore/commit/75992143c193ec6afdd5897e06eb3694fc80a13f
    
    Part of this Scripts are based in https://github.com/darkman1983/TrinityCore/commits/master
    
    Signed-off-by: burnham <georgiosburnham@gmail.com>

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5880,6 +5880,7 @@ void Spell::EffectDuel(SpellEffIndex effIndex)
     duel->opponent   = target;
     duel->startTime  = 0;
     duel->startTimer = 0;
+    duel->isMounted  = (GetSpellInfo()->Id == 62875); // Mounted Duel
     caster->duel     = duel;
 
     DuelInfo* duel2   = new DuelInfo;
@@ -5887,6 +5888,7 @@ void Spell::EffectDuel(SpellEffIndex effIndex)
     duel2->opponent   = caster;
     duel2->startTime  = 0;
     duel2->startTimer = 0;
+    duel2->isMounted  = (GetSpellInfo()->Id == 62875); // Mounted Duel
     target->duel      = duel2;
 
     caster->SetUInt64Value(PLAYER_DUEL_ARBITER, pGameObj->GetGUID());
